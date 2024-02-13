b9d45115d8e79817210f2f401292e0760ca7416f
Sambit Kumar Sahoo <sambit.sahoo@ajatus.co.in>
2024-02-14 00:20:31 +0530

    Added new project file

---
+++ Openbook/Openbook/Repository/Interface/IUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Openbook.Data;
+using Openbook.Data.Modules;
+using Openbook.Models;
+
+namespace Openbook.Repository.Interface
+{
+    public interface IUser
+    {
+        Task<int> Registro(RegistroViewModel model);
+        Task<ResponseMessage> RegistroCompany(RegistroViewModel model);
+        Task<bool> Login(LoginViewModel model);
+		IEnumerable<ApplicationUser> ViewUser();
+        List<UserCompany> ViewUserWithRole();
+        Task<ApplicationUser> EditUser(string id);
+        Task<bool> Update(ApplicationUser user);
+		List<IdentityRole> ViewRole();
+        bool Delete(string userId);
+        Task<ResponseMessage> ChangePassword(ChangePasswordModel model);
+
+	}
+}
